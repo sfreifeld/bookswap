59f926210b2efa757a3aca70e82711a93ab8a4e3
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-05-06 16:52:51 -0600

    started search

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
+import Event from "./Event";
+import Search from "./Search";
+
 
 function Home() {
-    // Fetch all ongoing events
     const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+
+    // Fetch all events
     useEffect(() => {
         fetch("/api/events") 
             .then(response => {
@@ -13,6 +18,7 @@ function Home() {
             })
             .then(data => {
                 setEvents(data);
+                setFilteredEvents(data);
             })
             .catch(error => {
                 console.error("Error fetching events:", error);
@@ -37,31 +43,25 @@ function Home() {
                     <p>Feeling inspired? Host your own book swap in minutes-- setting up event details and guidelines with ease. </p>
                 </div>    
                 <div className="flex flex-col p-2 sm:flex-row space-x-4">
-                    <a href="#" className="inline-flex items-center justify-center px-4 py-2.5 text-gray-600 text-center border border-gray-300 rounded-lg">
+                    <a href="#eventList" className="inline-flex items-center justify-center px-4 py-2.5 text-gray-600 text-center border border-gray-300 rounded-lg">
                         Browse Upcoming Events
                         <svg className="ml-2 -mr-1 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 7 4 4 4-4m-8 6 4 4 4-4"/></svg>
                     </a>  
-                    <a href="#" className="inline-flex items-center justify-center px-4 py-2.5 text-gray-600 text-center border border-gray-300 rounded-lg">
+                    <button className="inline-flex items-center justify-center px-4 py-2.5 text-gray-600 text-center border border-gray-300 rounded-lg">
                         Create Event
                         <svg className="ml-2 -mr-1 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clipRule="evenodd"/></svg>
-                    </a>  
+                    </button>  
                 </div>
             </section>
 
+            <section id="search">
+                <Search events={events} setFilteredEvents={setFilteredEvents} />
+            </section>
 
-            <section id="eventList"className="container space-y-10 mx-10">
-            <h2 className="text-4xl text-center font-bold">Upcoming Book Swap Events</h2>
-            
-                {events.map(event => (
-                    <div key={event.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-                        <div className="p-4">
-                            <h3 className="text-xl font-semibold mb-2">{event.name}</h3>
-                            <p>Date: {event.date}</p>
-                            <p>Address: {event.address}</p>
-                            <p>Attendees: {event.attendees}</p>
-                            <button>Click to view details</button>
-                        </div>
-                    </div>
+            <section id="eventList" className="container space-y-10 mx-10">
+                <h2 className="text-4xl text-center font-bold py-12">Upcoming Book Swap Events</h2>
+                    {filteredEvents.map(event => (
+                    <Event key={event.id} event={event} />
                 ))}
             </section>
         </>
@@ -69,3 +69,6 @@ function Home() {
 }
 
 export default Home;
+
+
+
