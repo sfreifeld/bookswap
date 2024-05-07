ad1cf0a2625b07c52a0ffd0c6e8cd12791289f85
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-05-07 15:19:03 -0600

    fixed merge issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,37 @@
-import Home from './components/Home'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import { useState } from 'react'
+
+import Signin from "./components/Signin"
+import Signup from "./components/Signup"
+import Home from "./components/Home"
+import Profile from "./components/Profile"
+
+
 import './App.css'
 import './tailwind.css'
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
 
   return (
-    <>
-      <Home />
-    </>
-  )
+    <Router>
+        <Routes>
+          <Route path="/signin" element={<Signin setUser={setUser}/>} />
+          <Route path="/home" element={<Home user={user} setUser={setUser}/>} /> 
+          <Route path="/createaccount" element={<Signup setUser={setUser}/>} /> 
+          <Route path="/profile" element={<Profile user={user} setUser={setUser}/>} />
+        </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
